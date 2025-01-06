b694cd53fadc75380f0d2284de234d3ff11355a1
MashedTaytors <taylor2120@outlook.com>
2025-01-06 10:56:27 -0600

    test comment getCookie

--- src/components/theme-toggle.jsx
+++ src/components/theme-toggle.jsx
@@ -8,12 +8,12 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 // TODO: on light theme full page load, the wrong toggle icon flashes
 // TODO: Hide theme toggle if no cookie consent is given
 
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-  return null;
-}
+// function getCookie(name) {
+//   const value = `; ${document.cookie}`;
+//   const parts = value.split(`; ${name}=`);
+//   if (parts.length === 2) return parts.pop().split(';').shift();
+//   return null;
+// }
 
 function setCookie(name, value, days) {
   const expires = new Date(Date.now() + days * 864e5).toUTCString();
