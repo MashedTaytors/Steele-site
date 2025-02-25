92ae34b0c029c82bb6b304db50c656641aa6035f
MashedTaytors <taylor2120@outlook.com>
2025-02-25 12:11:04 -0600

    update character counter placeholder text and height

--- src/app/projects/components/CharacterCounter.tsx
+++ src/app/projects/components/CharacterCounter.tsx
@@ -7,13 +7,13 @@ const CharacterCounter: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
   
   return (
-    <div className="flex flex-col min-h-72 w-full justify-between rounded-lg bg-transparent border border-solid border-gray-500 border-opacity-40 lg:flex-row">
+    <div className="flex flex-col min-h-64 w-full justify-between rounded-lg bg-transparent border border-solid border-gray-500 border-opacity-40 lg:flex-row">
       <div className="flex flex-col gap-2 p-6 w-full">
         <label className="font-sourceCodePro text-foreground text-sm"> Enter Text:</label>
         <textarea
           className="p-2 w-full rounded-md border border-gray-500 border-opacity-40 bg-transparent font-sourceCodePro focus:outline-none focus:border-opacity-100"
           rows={6}
-          placeholder="Enter URL"
+          placeholder="Type your message here..."
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
